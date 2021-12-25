f5dab7ac5094b2078e270b821f5bb4089ec0e3a4
JoshVL <jtvillanueva@wisc.edu>
2021-12-25 00:19:54 -0600

    :sparkles: Added technologies section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,54 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { FaTerminal, FaFigma, FaGitAlt } from 'react-icons/fa';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+    <SectionDivider />
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      Here's all the technologies and programming languages that I've learned throughout my undergrad experience and other applications!
+    </SectionText>
+    <List>
+      <ListItem>
+        <FaTerminal size="3rem" />
+        <ListContainer>
+          <ListTitle>Programming Languages</ListTitle>
+          <ListParagraph>
+            Python, Java, <br />
+            Javascript, <br />
+            C, C++, C# <br />
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <FaFigma size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Vue <br />
+            Three.JS <br />
+            React <br />
+            Figma <br />
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <FaGitAlt size="3rem" />
+        <ListContainer>
+          <ListTitle>Other Knowledge</ListTitle>
+          <ListParagraph>
+            Git <br />
+            Unity <br />
+            OS <br />
+            IT Help Desk <br />
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
