7cc50a77890a5c8b23e7b0463f183b4c2ee26b92
JoshVL <josh.villanueva@outlook.com>
2021-12-25 11:34:11 -0600

    :wrench: Renamed Timeline to About Me

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,7 +12,7 @@ const Hero = (props) => (
         I'm Josh Villanueva.
       </SectionTitle>
       <br />
-        <img src='/images/me.png' height="300" />
+        <img src='/images/me.png' height="300" width="300" />
       <br /><br />
       <SectionText>
       Heyo, I'm Josh! I'm a new graduate straight out of Wisconsin. I'm currently looking for opportunties to apply the skills and knowledge that I have obtained from studying at the University of Wisconsin - Madison and my student jobs working in IT. I'm interested in pursuing all areas of computer science, including frontend development, backend development, software engineering, and information technology and development areas.
