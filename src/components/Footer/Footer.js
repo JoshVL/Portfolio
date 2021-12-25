be07484bb63489eeecad7e146a7e37fb007df888
JoshVL <josh.villanueva@outlook.com>
2021-12-25 12:43:11 -0600

    :tada: Main functionality completed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,38 @@
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+import { RiGamepadFill } from 'react-icons/ri';
 
 import { SocialIcons } from '../Header/HeaderStyles';
 import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Phone</LinkTitle>
+          <LinkItem href="tel:414-429-9206">414-429-9206</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:jtvillanueva@wisc.edu">jtvillanueva@wisc.edu</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <LinkItem href="https://github.com/JoshVL/Portfolio"> View Source Code</LinkItem>
+        </CompanyContainer>
+        <SocialIcons href="https://github.com/JoshVL">
+          <AiFillGithub size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://joshvl.itch.io/">
+          <RiGamepadFill size="3rem" />
+        </SocialIcons>
+        <SocialIcons href="https://www.linkedin.com/in/joshua-villanueva-981411196/">
+          <AiFillLinkedin size="3rem" />
+        </SocialIcons>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
