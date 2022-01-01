03889b4dd4a9edc38f98eef40935565ae51f404e
Josh Villanueva <josh.villanueva@outlook.com>
2022-01-01 11:40:07 -0600

    :beers: Last Attempt For Button Fix

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -17,7 +17,7 @@ const Hero = (props) => (
       <SectionText>
       Heyo, I'm Josh! I'm a new graduate straight out of Wisconsin. I'm currently looking for opportunties to apply the skills and knowledge that I have obtained from studying at the University of Wisconsin - Madison and my student jobs working in IT. I'm interested in pursuing all areas of computer science, including frontend development, backend development, software engineering, and information technology and development areas.
       </SectionText>
-      <Button onClick={() => window.location = '/resume.pdf'}>
+      <Button onClick={() => window.open('https://joshvl.com/resume.pdf', '_blank')}>
         View Resume
       </Button>
     </LeftSection>
