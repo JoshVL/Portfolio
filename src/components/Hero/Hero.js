561ec92463ea9945a33fe1f21d113533f7b12213
Josh Villanueva <josh.villanueva@outlook.com>
2022-01-01 12:07:49 -0600

    :ambulance: Changed button redirect to href

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -17,9 +17,9 @@ const Hero = (props) => (
       <SectionText>
       Heyo, I'm Josh! I'm a new graduate straight out of Wisconsin. I'm currently looking for opportunties to apply the skills and knowledge that I have obtained from studying at the University of Wisconsin - Madison and my student jobs working in IT. I'm interested in pursuing all areas of computer science, including frontend development, backend development, software engineering, and information technology and development areas.
       </SectionText>
-      <Button onClick={() => window.open('https://joshvl.com/resume.pdf', '_blank')}>
-        View Resume
-      </Button>
+      <a href="https://joshvl.com/resume.pdf" target="_blank">
+        <Button> View Resume </Button>
+      </a>
     </LeftSection>
   </Section>
 );
